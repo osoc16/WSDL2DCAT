361a3576e5f3f185012ec70afecdd0b16c48bc14
mdstroop <migueldestrooper@hotmail.com>
2016-07-13 20:29:29 +0200

    Cleaned up project +changed bat-filefunctionality toconsole application

--- WSDL2DCAT/src/main/java/com/fedict/wsdl2dcat/WSDL2DCAT.java
+++ WSDL2DCAT/src/main/java/com/fedict/wsdl2dcat/WSDL2DCAT.java
@@ -6,6 +6,8 @@
 package com.fedict.wsdl2dcat;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -13,6 +15,7 @@ import java.io.InputStream;
  * @author Umut
  */
 public class WSDL2DCAT {
+
     static String inputDir;
     static String outputDir;
     static String stylesheetDir;
@@ -26,27 +29,50 @@ public class WSDL2DCAT {
      * @param args input, output, stylesheet directory
      */
     public static void main(String[] args) {
-        System.out.println("WSDL2DCAT tool \n");
+        Scanner sc = new Scanner(System.in);
         Converter converter = new Converter();
+        System.out.println("WSDL2DCAT tool \n");
+        boolean stop = false;
+        while (!stop) {
+            System.out.println("Select one of the following options:\n"
+                    + "1: start conversion with standard settings (default)\n"
+                    + "2: start conversion with custom settings\n"
+                    + "3: exit application\n"
+                    + "Option (default 1):");
+            String option = sc.nextLine();
+            if (option.equals("2")) {
 
-        switch (args.length) {
-            case 1:
-                //arguments: input directory
-                converter.convertToDCAT(args[0]);
-                break;
-            case 2:
-                //arguments: input directory, file type
-                converter.convertToDCAT(args[0], args[1]);
-                break;
-            case 3:
-                //arguments: input directory, file type , output directory
-                converter.convertToDCAT(args[0], args[1], args[2]);
-                break;
+                System.out.println("Input folder (default: files/Input/WSDL):");
+                String inputDir = sc.nextLine();
+                System.out.println(inputDir);
+                if (!inputDir.equals("")) {
+                    converter.setInputDir(inputDir);
+                }
+                    
+                System.out.println("File type (options:wsdl/xsd :: default: wsdl):");
+                String fileType = sc.nextLine();
+                while (!fileType.equalsIgnoreCase("wsdl") && !fileType.equalsIgnoreCase("xsd") && !fileType.equals("")) {
+                    System.out.println(fileType);
+                    System.out.println("Please provide one of the following filetypes: wsdl/xsd");
+                    fileType = sc.nextLine();
+                }
 
-            default:
+                if (!fileType.equals("")) {
+                    converter.setFileType(fileType);
+
+                }
+                System.out.println("outputfolder=Output folder (default: files/Output/DCAT):");
+                String outputDir = sc.nextLine();
+                if (!outputDir.equals("")) {
+                    converter.setInputDir(outputDir);
+                }
+                converter.convertToDCAT();
+            } else if (option.equals("3")) {
+                stop = true;
+            } else {
                 converter.convertToDCAT();
                 converter.convertFamiliesToDCAT();
-                break;
+            }
         }
     }
 
