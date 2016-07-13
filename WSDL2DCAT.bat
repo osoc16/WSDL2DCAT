51045dbc538f5eb02c785ec7df408e11cbc485cb
Izoman <umut.saglam1@hotmail.com>
2016-07-13 15:20:01 +0200

    Fixed error + made values reset

--- WSDL2DCAT.bat
+++ WSDL2DCAT.bat
@@ -6,14 +6,20 @@ SET filetype=wsdl
 SET outputfolder=files/Output/DCAT
 :Prerequire
 SET jarname=uber-WSDL2DCAT-1.0-SNAPSHOT.jar
-SET option=1
 SET /p jarname=Please provide the required JAR file: (uber-WSDL2DCAT-1.0-SNAPSHOT.jar) 
 IF exist "%jarname%" (
 	GOTO Start
 ) ELSE (
-	GOTO Errorjar
+	ECHO No JAR file provided. 
+	SET jarname=uber-WSDL2DCAT-1.0-SNAPSHOT.jar
+	GOTO Prerequire
 )
 :Start
+SET option=1
+SET mainclass=com.fedict.wsdl2dcat.WSDL2DCAT
+SET inputfolder=files/Input/WSDL
+SET filetype=wsdl
+SET outputfolder=files/Output/DCAT
 ECHO Select from the following options:
 ECHO 1: start conversion with standard settings (default)
 ECHO 2: start conversion with custom settings
@@ -35,13 +41,9 @@ GOTO Start
 	SET /p mainclass=Main class (default: com.fedict.wsdl2dcat.WSDL2DCAT):
 	SET /p inputfolder=Input folder (default: files/Input/WSDL):
 	SET /p filetype=File type (default: wsdl):
-	SET /p filetype=Output folder (default: files/Output/DCAT):
+	SET /p outputfolder=Output folder (default: files/Output/DCAT):
 	java -cp %jarname% %mainclass% %inputfolder% %filetype% %outputfolder%
 	PAUSE
 	GOTO Start
-:Errorjar
-	ECHO No JAR file provided. 
-	SET jarname=uber-WSDL2DCAT-1.0-SNAPSHOT.jar
-	GOTO Prerequire
 :End
 	ECHO Exiting application.
