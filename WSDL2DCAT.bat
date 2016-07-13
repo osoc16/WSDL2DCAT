aabd3446f912ed7bd4aae7973f2da54fe29907b7
Izoman <umut.saglam1@hotmail.com>
2016-07-13 13:11:23 +0200

    #18 Improved Bat file

--- WSDL2DCAT.bat
+++ WSDL2DCAT.bat
@@ -2,10 +2,10 @@
 ECHO Welcome to WSDL2DCAT app
 SET jarname=uber-WSDL2DCAT-1.0-SNAPSHOT.jar
 SET mainclass=com.fedict.wsdl2dcat.WSDL2DCAT
-SET inputfolder=Input
+SET inputfolder=files/Input/WSDL
 SET filetype=wsdl
-SET outputfolder=Output
-SET option="1"
+SET outputfolder=files/Output/DCAT
+SET option=1
 SET /p jarname=Please provide the required JAR file: (uber-WSDL2DCAT-1.0-SNAPSHOT.jar) 
 IF exist "%jarname%" (
 	GOTO Start
@@ -19,24 +19,23 @@ ECHO 2: start conversion with custom settings
 ECHO 3: exit application
 SET /p option=Option (default 1):
 ECHO Your option was: %option%
-IF "%option%" EQU "1" GOTO StartConvert
-IF "%option%" EQU "2" GOTO Customoption
+IF "%option%" EQU "1" GOTO DefaultConvert
+IF "%option%" EQU "2" GOTO CustomConvert
 IF "%option%" EQU "3" GOTO End
 
 ECHO Please provide valid option!
 PAUSE
 GOTO Start
-:StartConvert
+:DefaultConvert
+	java -cp %jarname% %mainclass%
+:CustomConvert
+	SET /p mainclass=Main class (default: com.fedict.wsdl2dcat.WSDL2DCAT):
+	SET /p inputfolder=Input folder (default: files/Input/WSDL):
+	SET /p filetype=File type (default: wsdl):
+	SET /p filetype=Output folder (default: files/Output/DCAT):
 	java -cp %jarname% %mainclass% %inputfolder% %filetype% %outputfolder%
 	PAUSE
 	GOTO Start
-:Customoption
-	SET /p mainclass=Main class (default: com.fedict.wsdl2dcat.WSDL2DCAT):
-	SET /p inputfolder=Input folder (default: Input):
-	SET /p filetype=File type (default: wsdl):
-	SET /p filetype=Output folder (default: Output):
-
-	GOTO StartConvert
 :Errorjar
 	ECHO No JAR file provided. 
 	PAUSE
